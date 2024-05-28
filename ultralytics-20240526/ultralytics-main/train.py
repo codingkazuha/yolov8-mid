cfb205d082bd48dfd4b8a4388cf694a3d302b51d
CodingKazuha <871244464@qq.com>
2024-05-28 23:48:18 +0800

    test

---
+++ ultralytics-20240526/ultralytics-main/train.py
@@ -0,0 +1,22 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/v8/yolov8n-C2f-EIEM.yaml')
+    # model.load('yolov8n.pt') # loading pretrain weights
+    model.train(data='/home/hjj/Desktop/dataset/dataset_crowdhuman/data_exp.yaml',
+                cache=True,
+                imgsz=640,
+                epochs=100,
+                batch=32,
+                close_mosaic=10,
+                workers=8,
+                device='0',
+                optimizer='SGD', # using SGD
+                # resume='', # last.pt path
+                # amp=False, # close amp
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
